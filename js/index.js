80fbce3b7d72fb12f3a868a9950d797bb18eb091
Saori Haga <ayaoriko107@gmail.com>
2019-10-22 01:53:16 +0900

    矢印修正、コンポーネント作成

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 console.log('読み込んだ！');
+(function () {
+    'use strict';
+
+    Vue.component('lesson-component',{
+        template:
+        '<section class="lesson"><div class="lesson_inner contents-inner"><div class="lesson_sub-title"></div><h2 class="lesson_title">{{ title }}</h2><p class="lesson_detail">{{ comment }}</p><figure class="lesson_img-wrap" :style="bgImage"></figure><div class="button_wrap"><a class="button_base" :href="slug">ページを見る</a></div></div></section>',
+        data: function(){
+            return{
+                bgImage: 'background-image: url(img/lesson_image.jpg)'
+            }
+        },
+        props: ['comment','slug','title']
+    })
+
+    var myComponent = {
+        template: '<p>MyComponent!!!</p>'
+    }
+
+    var vm = new Vue({
+        el: '#app',
+        components: {
+            'my-component': myComponent,
+        },
+        data: {
+            message: ''
+        }
+    });
+})();
