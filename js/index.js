bb3ef82b1472f1e0df04e1893de80f2cd1552114
Saori Haga <ayaoriko107@gmail.com>
2019-10-23 00:59:57 +0900

    コンポーネントを親子化

--- js/index.js
+++ js/index.js
@@ -4,13 +4,30 @@ console.log('読み込んだ！');
 
     Vue.component('lesson-component',{
         template:
-        '<section class="lesson"><div class="lesson_inner contents-inner"><div class="lesson_sub-title"></div><h2 class="lesson_title">{{ title }}</h2><p class="lesson_detail">{{ comment }}</p><figure class="lesson_img-wrap" :style="bgImage"></figure><div class="button_wrap"><a class="button_base" :href="slug">ページを見る</a></div></div></section>',
+        '<section class="lesson">\
+            <div class="lesson_inner contents-inner">\
+                <div class="lesson_sub-title"></div>\
+                <h2 class="lesson_title">{{ title }}</h2>\
+                <p class="lesson_detail">{{ comment }}</p>\
+                <figure class="lesson_img-wrap" :style="bgImage"></figure>\
+                <div class="button_wrap">\
+                    <a class="button_base" :href="url" @click="clickLink">ページを見る</a>\
+                </div>\
+            </div>\
+        </section>',
         data: function(){
             return{
-                bgImage: 'background-image: url(img/lesson_image.jpg)'
+                bgImage: 'background-image: url(img/'+ this.img +'.jpg)',
+                url: this.slug + '.html'
             }
         },
-        props: ['comment','slug','title']
+        methods: {
+            clickLink: function(){
+                console.log('click');
+                return false;
+            }
+        },
+        props: ['comment','slug','title','img']
     })
 
     var myComponent = {
@@ -23,7 +40,25 @@ console.log('読み込んだ！');
             'my-component': myComponent,
         },
         data: {
-            message: ''
+            list: [
+                {
+                    'title': 'Contact Form1',
+                    'comment': 'UIの基礎となるお問い合わせフォームをつくりました。',
+                    'img': 'lesson_image',
+                    'slug': 'contact-form',
+                },
+                {
+                    'title': 'Contact Form2',
+                    'comment': 'UIの基礎となるお問い合わせフォームをつくりました。',
+                    'img': 'contact-form',
+                    'slug': 'contact-form',
+                },
+                {
+                    'title': 'Contact Form3',
+                    'comment': 'UIの基礎となるお問い合わせフォームをつくりました。',
+                    'slug': 'contact-form',
+                }
+            ]
         }
     });
 })();
